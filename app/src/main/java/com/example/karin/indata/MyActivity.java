1d4f1ed16133348ee1bed880fc52babb9913b9b0
Karin R <karin_lr@hotmail.com>
2014-11-05 16:49:38 +0100

    Grund för att hämta data från internet i egen activity. Behöver AsyncTask för att funka.

--- app/src/main/java/com/example/karin/indata/MyActivity.java
+++ app/src/main/java/com/example/karin/indata/MyActivity.java
@@ -2,9 +2,23 @@ package com.example.karin.indata;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+// needs to use AsyncTask, can not get data via http in mail function.
+//http://developer.android.com/reference/android/os/AsyncTask.html
+//http://stackoverflow.com/questions/6343166/android-os-networkonmainthreadexception
 
 public class MyActivity extends Activity {
 
@@ -12,6 +26,8 @@ public class MyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        connect("http://dn.se"); // use the function below to download via http
+        Log.d("httpwtf", "Connectar" );
     }
 
 
@@ -33,4 +49,72 @@ public class MyActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    public static void connect(String url) //function for downloading data via http
+            // http://stackoverflow.com/questions/4457492/how-do-i-use-the-simple-http-client-in-android
+    {
+
+        HttpClient httpclient = new DefaultHttpClient();
+
+        // Prepare a request object
+        HttpGet httpget = new HttpGet(url);
+
+        // Execute the request
+        HttpResponse response;
+        try {
+            response = httpclient.execute(httpget);
+            // Examine the response status
+            Log.i("Praeda", response.getStatusLine().toString());
+
+            // Get hold of the response entity
+            HttpEntity entity = response.getEntity();
+            // If the response does not enclose an entity, there is no need
+            // to worry about connection release
+
+            if (entity != null) {
+
+                // A Simple JSON Response Read
+                InputStream instream = entity.getContent();
+                String result = convertStreamToString(instream);
+                // now you have the string representation of the HTML request
+                Log.d("httpwtf", "Hejpådaj" );
+                instream.close();
+            }
+
+
+        } catch (Exception e) {
+            Log.d("httpwtf", "Catchar" );
+            Log.d("httpwtf", e.toString() );
+        }
+    }
+
+    private static String convertStreamToString(InputStream is)  // belongs to fuction above
+    {
+    /*
+     * To convert the InputStream to String we use the BufferedReader.readLine()
+     * method. We iterate until the BufferedReader return null which means
+     * there's no more data to read. Each line will appended to a StringBuilder
+     * and returned as String.
+     */
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        Log.d("httpwtf", "bufferedreadear" );
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
 }
