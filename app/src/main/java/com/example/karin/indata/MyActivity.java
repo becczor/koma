5e82760d8727d9e590c63058c1dbbce51985735b
becczor <rebecca.lindblom@gmail.com>
2014-11-23 16:24:40 +0100

    Läser från fil, ändrar bild vid knapptryck, ny launcher icon

--- app/src/main/java/com/example/karin/indata/MyActivity.java
+++ app/src/main/java/com/example/karin/indata/MyActivity.java
@@ -2,34 +2,70 @@ package com.example.karin.indata;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import org.apache.http.*;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-// needs to use AsyncTask, can not get data via http in mail function.
-//http://developer.android.com/reference/android/os/AsyncTask.html
-//http://stackoverflow.com/questions/6343166/android-os-networkonmainthreadexception
+import java.io.*;
+import java.util.*;
 
 public class MyActivity extends Activity {
+
+    Scanner sc;
+    TextView rutan;
+    Button button;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-        connect("http://dn.se"); // use the function below to download via http
-        Log.d("httpwtf", "Connectar" );
-    }
 
+
+
+        image = (ImageView) findViewById(R.id.imageView);
+
+        button = (Button) findViewById(R.id.btnChangeImage);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                rutan = (TextView) findViewById(R.id.rutan); // import the textview into java code
+                Log.d("ReadFileLog", "Innan on-click");
+                BufferedReader br = null;
+
+                // when click on button, read from file and change textview and imageview
+                try {
+                    Log.d("ReadFileLog", "I try-satsen");
+                    InputStream iS = getAssets().open("data.txt");
+
+                    Log.d("ReadFileLog", "innan buffered reader");
+                    br = new BufferedReader(new InputStreamReader(iS));
+                    String line;
+                    Log.d("ReadFileLog", "Innan while");
+
+                    while((line = br.readLine()) != null)
+                    {
+                        Log.d("ReadFileLog", "I while-loopen");
+                        rutan.setText(line);
+                    }
+
+                    Log.d("ReadFileLog", "Det gick bra!");
+
+                    image.setVisibility(View.VISIBLE);
+                }
+                catch (Exception e)
+                {
+                    Log.d("ReadFileLog", "exception", e);
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,72 +85,4 @@ public class MyActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-
-    public static void connect(String url) //function for downloading data via http
-            // http://stackoverflow.com/questions/4457492/how-do-i-use-the-simple-http-client-in-android
-    {
-
-        HttpClient httpclient = new DefaultHttpClient();
-
-        // Prepare a request object
-        HttpGet httpget = new HttpGet(url);
-
-        // Execute the request
-        HttpResponse response;
-        try {
-            response = httpclient.execute(httpget);
-            // Examine the response status
-            Log.i("Praeda", response.getStatusLine().toString());
-
-            // Get hold of the response entity
-            HttpEntity entity = response.getEntity();
-            // If the response does not enclose an entity, there is no need
-            // to worry about connection release
-
-            if (entity != null) {
-
-                // A Simple JSON Response Read
-                InputStream instream = entity.getContent();
-                String result = convertStreamToString(instream);
-                // now you have the string representation of the HTML request
-                Log.d("httpwtf", "Hejpådaj" );
-                instream.close();
-            }
-
-
-        } catch (Exception e) {
-            Log.d("httpwtf", "Catchar" );
-            Log.d("httpwtf", e.toString() );
-        }
-    }
-
-    private static String convertStreamToString(InputStream is)  // belongs to fuction above
-    {
-    /*
-     * To convert the InputStream to String we use the BufferedReader.readLine()
-     * method. We iterate until the BufferedReader return null which means
-     * there's no more data to read. Each line will appended to a StringBuilder
-     * and returned as String.
-     */
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
-        Log.d("httpwtf", "bufferedreadear" );
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return sb.toString();
-    }
-
 }
